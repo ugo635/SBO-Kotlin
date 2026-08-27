bd3388fef51cfe0020a890187b2185a1ffc5674e
ugo635 <ugogiron2@gmail.com>
2026-08-27 13:36:51 +0200

    Fix

--- src/main/kotlin/net/sbo/mod/diana/DianaMobDetect.kt
+++ src/main/kotlin/net/sbo/mod/diana/DianaMobDetect.kt
@@ -276,8 +276,10 @@ object DianaMobDetect {
         if (id in defeated || id in warned) return
         if (!shouldAlertForMob(name)) return
         val hpThreshold = if (Diana.hpAlert > 0.0) Diana.hpAlert * 1_000_000 else 0.0
-        playCustomSound(SboDataObject.soundSettingsData.lowHpSound, SboDataObject.soundSettingsData.lowHpVolume) // Play the sound regardless of the hp alert setting
-        if (hpThreshold > 0.0 && health <= hpThreshold) {
+        val soundHpThreshold = if (Diana.soundHpAlert > 0.0) Diana.soundHpAlert * 1_000_000 else 0.0
+
+        if (health <= soundHpThreshold && soundHpThreshold > 0.0) playCustomSound(SboDataObject.soundSettingsData.lowHpSound, SboDataObject.soundSettingsData.lowHpVolume) // Play the sound regardless of the hp alert setting
+        if (health <= hpThreshold && hpThreshold > 0.0) {
             showTitle("§cHP LOW!", null, 10, 40, 10)
             warned.add(id)
         }
